0f06545740546c4f9b98893c33c8c3c775b553d7
goncalogbastos <bastos.g.goncalo@gmail.com>
2021-12-22 17:20:02 +0000

    update

---
+++ Civil3D_Plugins/Civil3D_Plugins.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+namespace Civil3D_Plugins
+{
+    public class Civil3D_Plugins
+    {
+        // Count Civil alignments
+        [CommandMethod("ca")]
+        public void count_alignments()
+        {
+            var creator = new Count_Alignments();
+            creator.Create();
+        }
+    }
+}
---
+++ Civil3D_Plugins/Count_Alignments.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+
+namespace Civil3D_Plugins
+{
+    public class Count_Alignments
+    {
+        public void Create()
+        {
+            CivilDocument doc = CivilApplication.ActiveDocument;
+            ObjectIdCollection alignments = doc.GetAlignmentIds();
+            string docInfo = string.Format($"\nHello World!\nThis document has {alignments.Count} alignments.\n");
+            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(docInfo);
+        }
+    }
+}
