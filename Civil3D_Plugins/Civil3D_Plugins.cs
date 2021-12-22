63c04caccbb53dfd2ae869392f4316bc80cd3831
goncalogbastos <bastos.g.goncalo@gmail.com>
2021-12-22 19:01:00 +0000

    update

--- Civil3D_Plugins/Civil3D_Plugins.cs
+++ Civil3D_Plugins/Civil3D_Plugins.cs
@@ -6,9 +6,9 @@ namespace Civil3D_Plugins
     {
         // Count Civil alignments
         [CommandMethod("ca")]
-        public void count_alignments()
+        public void create_alignment()
         {
-            var creator = new Count_Alignments();
+            var creator = new Create_Alignments();
             creator.Create();
         }
     }
---
+++ Civil3D_Plugins/Create_Alignments.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+
+// Credits: http://blog.civil3dreminders.com/2013/04/creating-alignments-with-net-api.html
+
+namespace Civil3D_Plugins
+{
+    public class Create_Alignments
+    {
+        public void Create()
+        {
+            var doc = CivilApplication.ActiveDocument;
+            var ed = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            // Ask the user to select a polyline to convert to an alignment
+            PromptEntityOptions opt = new PromptEntityOptions("\nSelect a polyline to convert to an Alignment");
+            opt.SetRejectMessage("\nObject must be a polyline.");
+            opt.AddAllowedClass(typeof(Polyline), false);
+            PromptEntityResult res = ed.GetEntity(opt);
+
+            // create some polyline options for creating the new alignment
+            PolylineOptions plops = new PolylineOptions();
+            plops.AddCurvesBetweenTangents = false;
+            plops.EraseExistingEntities = true;
+            plops.PlineId = res.ObjectId;
+
+            // uses an existing Alignment Style and Label Set Style
+            ObjectId testAlignmentID = Alignment.FromAcadObject(res.ObjectId);
+
+        }
+    }
+}
