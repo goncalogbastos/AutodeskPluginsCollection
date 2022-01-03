7674498a568865e0a720a6249357a963e4447d2a
goncalogbastos <bastos.g.goncalo@gmail.com>
2022-01-03 18:30:47 +0000

    surface_profile

--- Civil3D_Plugins/Surface_Profile.cs
+++ Civil3D_Plugins/Surface_Profile.cs
@@ -3,8 +3,10 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.Civil.ApplicationServices;
 using Autodesk.Civil.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using System;
 
 // Credits: https://adndevblog.typepad.com/infrastructure/2012/05/creating-a-profile-object-using-civil-3d-net-api.html
+// https://forums.autodesk.com/t5/civil-3d-customization/create-a-corridor-with-c/td-p/10230480
 
 namespace Civil3D_Plugins
 {
@@ -14,30 +16,49 @@ namespace Civil3D_Plugins
         {
             var civil_doc = CivilApplication.ActiveDocument;
             var ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            var db = Application.DocumentManager.MdiActiveDocument.Database;
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    // Ask the user to select an alignment
+                    PromptEntityOptions opt = new PromptEntityOptions("\nSelect an alignment:");
+                    opt.SetRejectMessage("\nObject must be an alignment.");
+                    opt.AddAllowedClass(typeof(Autodesk.Civil.DatabaseServices.Alignment), true);
+                    PromptEntityResult res = ed.GetEntity(opt);
+                    if (res.Status != PromptStatus.OK) return;
+                    ObjectId alignmentId = res.ObjectId;
+
+                    // Ask the user to select a surface
+                    PromptEntityOptions peo = new PromptEntityOptions("\nSelect a surface:");
+                    peo.SetRejectMessage("\nObject must be a surface.");
+                    peo.AddAllowedClass(typeof(Autodesk.Civil.DatabaseServices.TinSurface), true);
+                    PromptEntityResult result = ed.GetEntity(peo);
+                    if (result.Status != PromptStatus.OK) return;
+                    ObjectId surfaceid = result.ObjectId;
+
+                    // Get alignment properties
+                    Alignment alignment = tr.GetObject(alignmentId, OpenMode.ForRead) as Alignment;
+                    TinSurface surface = tr.GetObject(surfaceid, OpenMode.ForRead) as TinSurface;
+                    ObjectId layerId = alignment.LayerId;
+                    ObjectId styleId = civil_doc.Styles.ProfileStyles["COBA_TERRENO NATURAL"];
+                    ObjectId labelId = civil_doc.Styles.LabelSetStyles.ProfileLabelSetStyles["_Aucun affichage"];
+
+                    // Create Surface Profile
+                    System.Random random = new System.Random();
+                    double val = Math.Round((random.NextDouble()),2);                    
+                    ObjectId surfObjId = Profile.CreateFromSurface($"{surface.Name.ToString()}_{val}", alignmentId, surfaceid, layerId, styleId, labelId);
+
+                    tr.Commit();
+                }
+                catch (Autodesk.AutoCAD.Runtime.Exception ex)
+                {
+                    ed.WriteMessage("/n Exception message :" + ex.Message);
+                }
+            }
 
 
-            // Ask the user to select an alignment
-            PromptEntityOptions opt = new PromptEntityOptions("\nSelect an alignment:");
-            opt.SetRejectMessage("\nObject must be an alignment.");
-            opt.AddAllowedClass(typeof(Alignment), false);
-            PromptEntityResult res = ed.GetEntity(opt);
-            string alignmentName = res.StringResult;
-
-            // Ask the user to select a surface
-            PromptEntityOptions peo = new PromptEntityOptions("\nSelect a surface:");
-            peo.SetRejectMessage("\nObject must be a surface.");
-            peo.AddAllowedClass(typeof(Autodesk.Civil.DatabaseServices.Surface), false);
-            PromptEntityResult result = ed.GetEntity(opt);
-            string existing_ground = result.StringResult;
-
-            //string existing_ground = "Terreno_T3.2_PE_Uniao_Cart_PTs";
-            string profile_name = alignmentName + "_";
-            string lay_name = "0";
-            string style_name = "COBA_RASANTE";
-            string label_name = "COBA_ANOTAÇÃO_RASANTE";
-
-            ObjectId surfObjId = Profile.CreateFromSurface(profile_name, civil_doc, alignmentName, existing_ground, lay_name, style_name, label_name);
         }
     }
 }
-
