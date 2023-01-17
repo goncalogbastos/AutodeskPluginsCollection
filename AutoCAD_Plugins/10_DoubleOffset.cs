552606f0090a9643dfbc1302f7d98a65c4786887
goncalogbastos <bastos.g.goncalo@gmail.com>
2023-01-17 17:32:01 +0000

    DoubleOffset bug fix

--- AutoCAD_Plugins/10_DoubleOffset.cs
+++ AutoCAD_Plugins/10_DoubleOffset.cs
@@ -10,35 +10,38 @@ namespace AutoCAD_Plugins
         {
             var doc = Application.DocumentManager.MdiActiveDocument;
             var db = doc.Database;
-            var ed = Application.DocumentManager.MdiActiveDocument.Editor;
-
-            using (Transaction tr = db.TransactionManager.StartTransaction())
+            var ed = Application.DocumentManager.MdiActiveDocument.Editor;            
+            
+            try
             {
-                try
+                // Prompt the user for the offset
+                PromptDoubleOptions pdo = new PromptDoubleOptions("\nEnter offset: ");
+                PromptDoubleResult offset_ = ed.GetDouble(pdo);
+
+                var running = true;
+                while (running)
                 {
-                    // Prompt the user for the offset
-                    PromptDoubleOptions pdo = new PromptDoubleOptions("\nEnter offset: ");
-                    PromptDoubleResult offset_ = ed.GetDouble(pdo);
+                    // Ask the user to select a polyline 
+                    PromptEntityOptions opt = new PromptEntityOptions("\nSelect a polyline: ");
+                    opt.SetRejectMessage("\nObject must be a polyline.");
+                    opt.AddAllowedClass(typeof(Polyline), true);
+                    opt.AddAllowedClass(typeof(Polyline), true);
+                    PromptEntityResult res = ed.GetEntity(opt);
 
-                    var running = true;
-                    while (running)
+                    if (res.Status != PromptStatus.OK)
                     {
-                        // Ask the user to select a polyline 
-                        PromptEntityOptions opt = new PromptEntityOptions("\nSelect a polyline: ");
-                        opt.SetRejectMessage("\nObject must be a polyline.");
-                        opt.AddAllowedClass(typeof(Polyline), true);
-                        opt.AddAllowedClass(typeof(Polyline), true);
-                        PromptEntityResult res = ed.GetEntity(opt);
+                        running = false;
+                        return;
+                    }
 
-                        if (res.Status != PromptStatus.OK)
-                        {
-                            running = false;
-                            return;
-                        }
-                        
+                    using (Transaction tr = db.TransactionManager.StartTransaction())
+                    {
                         // Get the polyline selected
                         Polyline pl = tr.GetObject(res.ObjectId, OpenMode.ForRead) as Polyline;
-                        ed.WriteMessage(pl.ObjectId.ToString());
+
+                        // Offset the polyline a given distance
+                        DBObjectCollection pline_offset_plus = pl.GetOffsetCurves(offset_.Value);
+                        DBObjectCollection pline_offset_minus = pl.GetOffsetCurves(-1 * offset_.Value);
 
                         // Open the Block table for read
                         BlockTable block_table;
@@ -47,10 +50,6 @@ namespace AutoCAD_Plugins
                         // Open the Block table record Model space for write
                         BlockTableRecord block_table_record;
                         block_table_record = tr.GetObject(block_table[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-
-                        // Offset the polyline a given distance
-                        DBObjectCollection pline_offset_plus = pl.GetOffsetCurves(offset_.Value);
-                        DBObjectCollection pline_offset_minus = pl.GetOffsetCurves(-1 * offset_.Value);
 
                         // Step through the new objects created
                         foreach (Entity ent in pline_offset_plus)
@@ -66,18 +65,17 @@ namespace AutoCAD_Plugins
                             block_table_record.AppendEntity(ent);
                             tr.AddNewlyCreatedDBObject(ent, true);
                         }
-                        
-                        
-                    }
-                   
-                }
-                catch (Autodesk.AutoCAD.Runtime.Exception ex)
-                {
-                    ed.WriteMessage("\nException message :" + ex.Message);
-                }
+                        tr.Commit();
+                    }                        
 
-                tr.Commit();
+                }                   
+
             }
+            catch (Autodesk.AutoCAD.Runtime.Exception ex)
+            {
+                ed.WriteMessage("\nException message :" + ex.Message);
+            }            
+            
         }
     }
 }
